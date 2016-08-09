86a88605b8f58967ce40ea7daacc1a30de5c89b3
Robert Strube <rstrube@gmail.com>
2016-08-09 02:42:20 -0400

    initial commit

---
+++ _posts/blog/2016-08-05-restfulbrilliance-com-is-live.markdown
@@ -0,0 +1,19 @@
+---
+layout: post
+title:  "Restfulbrilliance.com is Live!"
+date:   2016-08-04 18:00:00 -0400
+tags: announcements
+hero_img: "/img/hero/light-house-sunset-2200-1200.jpg"
+---
+
+We're extremely happy that Restfulbrilliance.com has finally gone live!  We've got some exciting new projects on the horizon that we're looking forward to sharing with everybody.
+
+For those of you that are interested, the site is built using a home-grown framework which cobbles together some really fantastic technologies.  Specifically:
+
+1. Zurb Foundation
+2. Jekyll
+3. A build system which uses: NPM, Bower, Gulp, and Browsersync
+
+In addition we wanted to make the framework easy to use with Visual Studio.
+
+We are currently hoping to release a more generic version the framework that others can use it to build sites and blogs.  Look for more information to be released shortly.
---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+/*------------------------------------------------------------------
+
+    Description: Gulpfile for Foundation + Jekyll
+    Author: Robert Strube
+    Version: 1.0
+
+-------------------------------------------------------------------*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoPrefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var child = require('child_process');
+var util = require('gulp-util');
+var browserSync = require('browser-sync').create();
+var mergeStream = require('merge-stream');
+
+var foundationSassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+var jekyllExec = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+
+gulp.task('sass-compile', function (gulpCallBack) {
+
+    var sassStream = gulp.src('_sass/compiled-foundation.scss')
+      .pipe(sass({ includePaths: foundationSassPaths, outputStyle: 'expanded' })
+      .on('error', sass.logError));
+
+    var cssStream = gulp.src(['css/**/*.css']);
+
+    return mergeStream(sassStream, cssStream)
+      .pipe(autoPrefixer({ browsers: ['last 2 versions', 'ie >= 9'] }))
+      .pipe(gulp.dest('_site/css/'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('_site/css/'))
+      .pipe(browserSync.stream());
+
+    gulpCallBack(null);
+});
+
+gulp.task('js-compile', function (gulpCallBack) {
+    return gulp.src(['bower_components/jquery/dist/jquery.js',
+                     'bower_components/what-input/what-input.js',
+                     'bower_components/foundation-sites/dist/foundation.js',
+                     'bower_components/jquery-cookie/jquery.cookie.js',
+                     'js/**/*.js'])
+      .pipe(gulp.dest('_site/js/'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(uglify())
+      .pipe(gulp.dest('_site/js/'));
+
+    gulpCallBack(null);
+});
+
+gulp.task('jekyll-clean', function (gulpCallBack) {
+
+    var jekyll = child.spawn(jekyllExec, ['clean']);
+
+    var jekyllLogger = (buffer) => {
+        buffer.toString()
+          .split(/\n/)
+          .forEach((message) => util.log('Jekyll: ' + message));
+    };
+
+    jekyll.stdout.on('data', jekyllLogger);
+    jekyll.stderr.on('data', jekyllLogger);
+
+    jekyll.on('exit', function (code) {
+
+        if (code === 0) {
+            util.log('Jekyll: [clean] Exited Successfully');
+            gulpCallBack(null);
+        }
+
+        else {
+            gulpCallBack('Jekyll: [clean] Exited with Error Code = ' + code);
+        }
+    });
+});
+
+gulp.task('jekyll-build', ['sass-compile', 'js-compile'], function (gulpCallBack) {
+
+    var jekyll = child.spawn(jekyllExec, ['build']);
+
+    var jekyllLogger = function (buffer) {
+        buffer.toString()
+          .split(/\n/)
+          .forEach((message) => util.log('Jekyll: ' + message));
+    };
+
+    jekyll.stdout.on('data', jekyllLogger);
+    jekyll.stderr.on('data', jekyllLogger);
+
+    jekyll.on('exit', function (code) {
+
+        if (code === 0){
+            util.log('Jekyll: [build] Exited Successfully');
+            gulpCallBack(null);
+        }
+
+        else
+        {
+            gulpCallBack('Jekyll: [build] Exited with Error Code = ' + code);
+        }
+    });
+});
+
+gulp.task('browser-sync-js-reload', ['js-compile'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync-html-reload', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync-serve', ['jekyll-build'], function () {
+
+    browserSync.init({
+        server: {
+            baseDir: './_site'
+        }
+    });
+
+    gulp.watch(['_layouts/**/*.html',
+                '_includes/**/*.html',
+                '_posts/**/*.md',
+                '_posts/**/*.markdown',
+                '*.md',
+                '*.html',
+                'blog/*.html',
+                'img/**/*.*'], ['browser-sync-html-reload']);
+    gulp.watch(['_sass/**/*.scss',
+                'css/**/*.css'], ['sass-compile']);
+    gulp.watch(['js/**/*.js'], ['browser-sync-js-reload']);
+});
+
+gulp.task('default', ['browser-sync-serve'], function () {
+});
